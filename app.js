e2a876e9695395f6c341cf02f953ebcb884380e3
Ganeshk750 <ganesh9ot@gmail.com>
2019-12-20 22:35:57 +0530

    addind public floder and html file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = reqire('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyPaser = require('body-parser');
 
@@ -9,10 +9,14 @@ const bodyPaser = require('body-parser');
 app.use(bodyPaser.json());
 app.use(cors());
 
+//api
+app.get('/new/:urlShort(*)',(req,res,next) =>{
+    const {urlShort} = req.params;
+    console.log(urlShort);
+});
 
 
-
-const post = process.env.PORT || 3000;
-app.listen(post,() =>{
+const port = process.env.PORT || 3000;
+app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
-})
+});
